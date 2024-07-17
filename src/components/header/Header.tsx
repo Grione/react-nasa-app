b77435a7ab31c094fe6b6887a9b45facc29a5ed8
Grigoriy Gavrilov <greeb.spb@gmail.com>
2024-07-17 15:19:38 +0500

    add styles

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,13 +1,16 @@
 import { NavLink } from "react-router-dom";
+import classes from './Header.module.css';
 
 const Header = () => {
+
   return (
-    <header>
-      <span>COSMOS</span>
-      <nav>
+    <header className={classes.header}>
+      <span className={classes.slogan}>COSMOS</span>
+      <nav className={classes.nav}>
         <ul>
-          <li><NavLink to="/">Main</NavLink></li>
-          <li><NavLink to="/explorer">Start explorer</NavLink></li>
+          <li><NavLink to="/"
+            className={({ isActive }) => isActive ? `${classes['active']} ${classes['nav-link']}` : classes['nav-link']}>Main</NavLink></li>
+          <li><NavLink to="/explorer" className={({ isActive }) => isActive ? `${classes['active']} ${classes['nav-link']}` : classes['nav-link']}>Start explorer</NavLink></li>
         </ul>
       </nav>
     </header>
